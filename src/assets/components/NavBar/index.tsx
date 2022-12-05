d7ae576968b609706f3ab924354cf18fb5cdd4f0
patrickwarley <patrickwarley@gmail.com>
2022-12-04 21:54:34 -0300

    Could not do much I just don't know where to start

---
+++ src/assets/components/NavBar/index.tsx
@@ -0,0 +1,9 @@
+import { StyledNav } from './styles';
+
+function NavBar() {
+  return (
+    <StyledNav className="" >Patick</StyledNav>
+  );
+}
+
+export default NavBar;
---
+++ src/assets/components/NavBar/styles.tsx
@@ -0,0 +1,10 @@
+import { styled } from "@stitches/react";
+
+export const StyledNav = styled(
+  'nav', {
+  backgroundColor: 'black',
+  color: 'white',
+  width: '100vw',
+  padding: '20px'
+}
+);
