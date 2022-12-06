a66e14108d97875a436fa2192b3b821cfd530745
patrickWarley <patrickwarley@gmail.com>
2022-12-06 19:48:27 -0300

    Nothing new

--- src/assets/components/NavBar/index.tsx
+++ src/assets/components/NavBar/index.tsx
@@ -2,7 +2,11 @@ import { StyledNav } from './styles';
 
 function NavBar() {
   return (
-    <StyledNav className="" >Patick</StyledNav>
+    <StyledNav className="" >
+      <a href='#'> Para começar a desenvolver eu preciso definir que serviços a empresa vai prestar </a>
+      <button>Entrar</button>
+      <button>Inscreva-se gratuitamente</button>
+    </StyledNav>
   );
 }
 
